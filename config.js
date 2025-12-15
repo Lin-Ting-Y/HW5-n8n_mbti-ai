2acfd5175c37bd7aa235e8964cf8974a0bbc2269
Ting Yu Lin <lintyu3919@gmail.com>
2025-12-15 13:44:22 +0800

    Force add config.js for deployment

---
+++ config.js
@@ -0,0 +1,5 @@
+// 將此檔案複製為 config.js 並填入實際的 Webhook URL
+// 建議不要將 config.js 納入版控，以保護敏感資訊
+window.__APP_CONFIG__ = {
+    N8N_WEBHOOK_URL: "https://lintingyu.app.n8n.cloud/webhook/f2318528-f36e-48a0-9f7c-784ac521c111"
+};
